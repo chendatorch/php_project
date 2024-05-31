2d21e4560bbb4f643e9df814b913344546e37d52
chendatorch <chendatorch@gmail.com>
2024-05-31 18:08:51 +0700

    first commit

---
+++ home.php
@@ -0,0 +1,136 @@
+<style>
+	.custom-menu {
+        z-index: 1000;
+	    position: absolute;
+	    background-color: #ffffff;
+	    border: 1px solid #0000001c;
+	    border-radius: 5px;
+	    padding: 8px;
+	    min-width: 13vw;
+}
+a.custom-menu-list {
+    width: 100%;
+    display: flex;
+    color: #4c4b4b;
+    font-weight: 600;
+    font-size: 1em;
+    padding: 1px 11px;
+}
+	span.card-icon {
+    position: absolute;
+    font-size: 3em;
+    bottom: .2em;
+    color: #ffffff80;
+}
+.file-item{
+	cursor: pointer;
+}
+a.custom-menu-list:hover,.file-item:hover,.file-item.active {
+    background: #80808024;
+}
+table th,td{
+	/*border-left:1px solid gray;*/
+}
+a.custom-menu-list span.icon{
+		width:1em;
+		margin-right: 5px
+}
+.candidate {
+    margin: auto;
+    width: 23vw;
+    padding: 0 10px;
+    border-radius: 20px;
+    margin-bottom: 1em;
+    display: flex;
+    border: 3px solid #00000008;
+    background: #8080801a;
+
+}
+.candidate_name {
+    margin: 8px;
+    margin-left: 3.4em;
+    margin-right: 3em;
+    width: 100%;
+}
+	.img-field {
+	    display: flex;
+	    height: 8vh;
+	    width: 4.3vw;
+	    padding: .3em;
+	    background: #80808047;
+	    border-radius: 50%;
+	    position: absolute;
+	    left: -.7em;
+	    top: -.7em;
+	}
+	
+	.candidate img {
+    height: 100%;
+    width: 100%;
+    margin: auto;
+    border-radius: 50%;
+}
+.vote-field {
+    position: absolute;
+    right: 0;
+    bottom: -.4em;
+}
+</style>
+
+<div class="containe-fluid">
+
+<div class="row">
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-primary text-white mb-4">
+                                    <div class="card-body">Orders</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="index.php?page=orders">View Details</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-warning text-white mb-4">
+                                    <div class="card-body">Products</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="index.php?page=products_image">View Details</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-success text-white mb-4">
+                                    <div class="card-body">User</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="index.php?page=users">View Details</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-danger text-white mb-4">
+                                    <div class="card-body">Discount</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="dis/index_dis.php">View Details</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+						
+<?php //var_dump($_SESSION)  ?>
+									
+				</div>
+			</div>
+			
+		</div>
+		</div>
+	</div>
+
+</div>
+<script>
+
+	
+</script>
